8b621ec52420e5ac67b979746f9243b7786b3313
mpoprawa <mateusz.poprawa@gmail.com>
2021-10-13 19:44:44 +0200

    basic setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CommentBox from './CommentBox'
+import CommentlList from './CommentlList'
+
+const App = () => {
+	return (
+		<div>
+			<CommentBox />
+			<CommentlList />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/CommentlList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CommentlList = () => {
+	return <div>Comment List</div>
+}
+
+export default CommentlList
